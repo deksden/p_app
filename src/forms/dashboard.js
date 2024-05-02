53338cfa9040673641fab6d6b533ca610d3fe246
Denis Kiselev <deksden@deksden.com>
2024-05-02 23:00:22 +0700

    [dashboard] : generate reports button and some notifications

--- src/forms/dashboard.js
+++ src/forms/dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
@@ -6,6 +6,8 @@ import CardHeader from '@material-ui/core/CardHeader'
 import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles'
 import { CHANGE_THEME, ThemeName } from '../core/actions'
+import { useNotify, fetchStart, fetchEnd, fetchUtils } from 'react-admin'
+import { Headers } from 'node-fetch'
 
 const changeTheme = (theme) => ({
   type: CHANGE_THEME,
@@ -18,35 +20,80 @@ const useStyles = makeStyles({
 })
 
 const Dashboard = () => {
+  const { fetchJson } = fetchUtils
+  const fetch = fetchJson
   const theme = useSelector((state) => state.theme)
   const dispatch = useDispatch()
   const classes = useStyles()
+  // const redirect = useRedirect()
+  const notify = useNotify()
+  const [loading, setLoading] = useState(false)
+  const ApiUrl = process.env.REACT_APP_API_PATH
+  const token = window.localStorage.getItem('token')
+  const options = { method: 'GET' }
+
+  if (token) {
+    options.headers = new Headers({ Authorization: `Bearer ${token}` })
+  }
+
+  const onReports = () => {
+    setLoading(true)
+    dispatch(fetchStart()) // start the global loading indicator
+    // const updatedRecord = { ...record, is_approved: true };
+    // fetch(`/comments/${record.id}`, { method: 'PUT', body: updatedRecord })
+    fetch(`${ApiUrl}/mrp/reports/all`, options)
+      .then(() => {
+        notify('Reports generated')
+        // redirect('/comments')
+      })
+      .catch((e) => {
+        notify('Error: report generation failed', { type: 'error' })
+      })
+      .finally(() => {
+        setLoading(false)
+        dispatch(fetchEnd()) // stop the global loading indicator
+      })
+  }
 
   return (
-    <Card>
-      <CardHeader title='Welcome to GoodCar.rent APP!' />
-      <CardContent>
-        <div className={classes.label}>
-          Theme:
-        </div>
+    <>
+      <Card>
+        <CardHeader title='Welcome to APP!' />
+        <CardContent>
+          <div className={classes.label}>
+            Theme:
+          </div>
+          <Button
+            variant='contained'
+            className={classes.button}
+            color={theme === 'light' ? 'primary' : 'default'}
+            onClick={() => dispatch(changeTheme(ThemeName.light))}
+          >
+            Light
+          </Button>
+          <Button
+            variant='contained'
+            className={classes.button}
+            color={theme === 'dark' ? 'primary' : 'default'}
+            onClick={() => dispatch(changeTheme(ThemeName.dark))}
+          >
+            Dark
+          </Button>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader title='App actions:' />
         <Button
           variant='contained'
           className={classes.button}
           color={theme === 'light' ? 'primary' : 'default'}
-          onClick={() => dispatch(changeTheme(ThemeName.light))}
+          onClick={onReports}
+          disabled={loading}
         >
-          Light
+          Generate reports
         </Button>
-        <Button
-          variant='contained'
-          className={classes.button}
-          color={theme === 'dark' ? 'primary' : 'default'}
-          onClick={() => dispatch(changeTheme(ThemeName.dark))}
-        >
-          Dark
-        </Button>
-      </CardContent>
-    </Card>
+      </Card>
+    </>
   )
 }
 
