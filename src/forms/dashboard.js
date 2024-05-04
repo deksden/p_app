6106f0d7088fd73bf9f0b0f89eae3b2e4914e622
Denis Kiselev <deksden@deksden.com>
2024-05-04 13:34:57 +0700

    [dashboard] : variantFolder support

--- src/forms/dashboard.js
+++ src/forms/dashboard.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardHeader from '@material-ui/core/CardHeader'
 import Button from '@material-ui/core/Button'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
 import { makeStyles } from '@material-ui/core/styles'
 // import { CHANGE_THEME, ThemeName } from '../core/actions'
-import { useNotify, fetchStart, fetchEnd, fetchUtils } from 'react-admin'
+import {
+  useNotify, fetchStart, fetchEnd, fetchUtils,
+  Loading
+} from 'react-admin'
 import { Headers } from 'node-fetch'
 
 // const changeTheme = (theme) => ({
@@ -14,26 +19,74 @@ import { Headers } from 'node-fetch'
 //   payload: theme
 // })
 
+const DEF_FOLDER = '(Основной)'
+const ApiUrl = process.env.REACT_APP_API_PATH
+const { fetchJson } = fetchUtils
+const fetch = fetchJson
+
 const useStyles = makeStyles({
   label: { width: '10em', display: 'inline-block' },
   button: { margin: '1em' }
 })
 
 const Dashboard = () => {
-  const { fetchJson } = fetchUtils
-  const fetch = fetchJson
   const theme = useSelector((state) => state.theme)
   const dispatch = useDispatch()
   const classes = useStyles()
   // const redirect = useRedirect()
   const notify = useNotify()
   const [loading, setLoading] = useState(false)
-  const ApiUrl = process.env.REACT_APP_API_PATH
   const token = window.localStorage.getItem('token')
   const options = { method: 'GET' }
+  const [folder, setFolder] = useState('(Основной)')
 
   if (token) {
     options.headers = new Headers({ Authorization: `Bearer ${token}` })
+  }
+
+  const VariantFolders = () => {
+  // const dataProvider = useDataProvider()
+    const [variantFolders, setVariantFolders] = useState()
+    // const [folder, setFolder] = useState(DEF_FOLDER)
+    // const [loading, setLoading] = useState(true)
+    const [error, setError] = useState()
+    useEffect(() => {
+      fetch(`${ApiUrl}/seed/variant-folders`)
+      // dataProvider.getMany('/seed/variant-folders')
+        .then((ret) => {
+          console.log('data get')
+          console.log(`data: ${JSON.stringify(ret.json)}`)
+          setVariantFolders(ret.json)
+          setLoading(false)
+        })
+        .catch(error => {
+          console.log(`error: ${JSON.stringify(error)}`)
+          setError(error)
+          setLoading(false)
+        })
+    }, [])
+
+    const handleChange = (event) => {
+      setFolder(event.target.value)
+    }
+
+    if (loading) return <Loading />
+    if (error) return <p>ERROR: {JSON.stringify(error)} </p>
+    if (!variantFolders || !Array.isArray(variantFolders)) return null
+
+    return (
+      <Select
+        labelId='folder-select'
+        id='folder-select'
+        value={folder}
+        onChange={handleChange}
+      >
+        <MenuItem key='none' value={DEF_FOLDER}>{DEF_FOLDER}</MenuItem>
+        {variantFolders.map(aFolder =>
+          <MenuItem key={aFolder} value={aFolder}>{aFolder}</MenuItem>
+        )}
+      </Select>
+    )
   }
 
   const onReports = () => {
@@ -55,12 +108,16 @@ const Dashboard = () => {
       })
   }
 
-  const onPlanAll = () => {
+  const onPlanAll = (aFolder) => {
+    console.log(`current folder is ${aFolder}`)
+    let params = ''
+    if (aFolder !== DEF_FOLDER) params = `?version=${aFolder}`
+    console.log(`params=${params}`)
     setLoading(true)
     dispatch(fetchStart()) // start the global loading indicator
     // const updatedRecord = { ...record, is_approved: true };
     // fetch(`/comments/${record.id}`, { method: 'PUT', body: updatedRecord })
-    fetch(`${ApiUrl}/mrp/plan`, options)
+    fetch(`${ApiUrl}/mrp/plan${params}`, options)
       .then(() => {
         notify('Планирование выполнено')
         // redirect('/comments')
@@ -107,7 +164,7 @@ const Dashboard = () => {
             variant='contained'
             className={classes.button}
             color={theme === 'light' ? 'primary' : 'default'}
-            onClick={onPlanAll}
+            onClick={() => onPlanAll(folder)}
             disabled={loading}
           >
           Планировать
@@ -121,6 +178,12 @@ const Dashboard = () => {
           >
           XLSX отчеты
           </Button>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader title='Вариант данных:' />
+        <CardContent>
+          <VariantFolders />
         </CardContent>
       </Card>
     </>
